9d76d3e73da6e10f4f30c1efb6c61e6de9f21061
jiyu <pllpopogg@gmail.com>
2026-05-28 21:47:08 +0900

    2-6

--- code/2-6.c
+++ code/2-6.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, const char* argv[]) {
+
+int main(int argc, const char* argv[]) 
+{
     if (argc < 3)
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
     const char* target_str = argv[2];
-
-
     
-    fclose(fp);
-}
+    char line[128];
+    while (fgets(line, sizeof line, fp)) {
+        if (strstr(line, argv[2])) { //포함하면
+            printf("%s", line); //출력
+        }
+    }
 
+    fclose(fp);
+
+    return 0;
+}
