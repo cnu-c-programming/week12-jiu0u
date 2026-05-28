62bb23166f2ba66d697f3c6239bc25b072db1436
jiyu <pllpopogg@gmail.com>
2026-05-28 15:40:19 +0900

    2-2

--- code/2-2.c
+++ code/2-2.c
@@ -1,28 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <string.h>
+
 
 typedef struct student {
     char name[64];
     int score;
 } Student;
 
-int main(int argc, const char* argv[]) {
+
+int main(int argc, const char* argv[]) 
+{
     FILE* fp = fopen("student.txt", "r");
     int count = 0;
-    Student students[64];
+    Student student[64];
 
+    while (count < 64) {
+        fscanf(fp, "%s %d", student[count].name, &student[count].score);
 
+        if (feof(fp))
+            break;
+
+        count++;
+    }
 
     int max = 0;
     float avg = 0;
 
+    int sum = 0;
+    max = student[0].score;
 
+    for (int i = 0; i < count; i++) {
+        sum += student[i].score;
+        if (max < student[i].score)
+            max = student[i].score;
+    }
+    avg = (float)sum / count;
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
-
+    
     fclose(fp);
 
     return 0;
 }
-
